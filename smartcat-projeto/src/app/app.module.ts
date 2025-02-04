55c6c3b484669df2c02572bf6335a3f0e42c350a
Neraky <felipecartaxo1@gmail.com>
2025-02-04 15:10:25 -0300

    desenvolvimento do menu lateral

--- smartcat-projeto/src/app/app.module.ts
+++ smartcat-projeto/src/app/app.module.ts
@@ -3,16 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuLateralComponent } from './components/menu-lateral/menu-lateral.component';
+
+// Importando os módulos do Angular Material
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatList, MatListItem } from '@angular/material/list';
+import { MatSidenavContainer } from '@angular/material/sidenav'; //
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuLateralComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    MatList,
+    MatListItem,
+    MatSidenavContainer
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
