be1f6279f41d18baf128c148c37da06c5445e479
Neraky <felipecartaxo1@gmail.com>
2025-02-08 19:16:44 -0300

    atualização dos imports dos módulos do Angular Material

--- smartcat-projeto/src/app/tarefa/tarefa.module.ts
+++ smartcat-projeto/src/app/tarefa/tarefa.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { ListagemTarefasComponent } from './listagem-tarefas/listagem-tarefas.component';
+import {MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardSubtitle} from '@angular/material/card';
+import {MatIcon} from '@angular/material/icon';
+import {MatBadge} from '@angular/material/badge';
+import {MatCardTitle} from '@angular/material/card';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+    declarations: [
+        ListagemTarefasComponent
+    ],
+    exports: [
+        ListagemTarefasComponent
+    ],
+    imports: [
+        CommonModule,
+        MatCard,
+        MatCardHeader,
+        MatCardContent,
+        MatCardActions,
+        MatIcon,
+        MatBadge,
+        MatCardTitle,
+        MatCardSubtitle
+    ]
 })
 export class TarefaModule { }
