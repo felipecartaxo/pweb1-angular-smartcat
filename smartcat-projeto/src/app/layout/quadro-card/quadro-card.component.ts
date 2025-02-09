ef3c6c83de8699d53c40d05f982a782edd20a0dd
Neraky <felipecartaxo1@gmail.com>
2025-02-09 13:37:10 -0300

    criando e editando componente referentes aos cards da aplicação

---
+++ smartcat-projeto/src/app/layout/quadro-card/quadro-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quadro-card',
+  standalone: false,
+  
+  templateUrl: './quadro-card.component.html',
+  styleUrl: './quadro-card.component.css'
+})
+export class QuadroCardComponent {
+
+}
