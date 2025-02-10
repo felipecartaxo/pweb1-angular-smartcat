043864d3cd5522e15550b3b4b588c3dd0402c220
Neraky <felipecartaxo1@gmail.com>
2025-02-10 20:23:24 -0300

    atualização do projeto com os componentes de listagem e cadastro

--- smartcat-projeto/src/app/tarefa/tarefa.module.ts
+++ smartcat-projeto/src/app/tarefa/tarefa.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListagemTarefasComponent } from './listagem-tarefas/listagem-tarefas.component';
-import {MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardSubtitle} from '@angular/material/card';
-import {MatIcon} from '@angular/material/icon';
-import {MatBadge} from '@angular/material/badge';
-import {MatCardTitle} from '@angular/material/card';
+import { MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardSubtitle } from '@angular/material/card';
+import { MatIcon } from '@angular/material/icon';
+import { MatBadge } from '@angular/material/badge';
+import { MatCardTitle } from '@angular/material/card';
+import { MatButton } from '@angular/material/button';
+import { CadastroTarefasComponent } from './cadastro-tarefas/cadastro-tarefas.component';
 
 @NgModule({
     declarations: [
-        ListagemTarefasComponent
+        ListagemTarefasComponent,
+        CadastroTarefasComponent
     ],
     exports: [
-        ListagemTarefasComponent
+        ListagemTarefasComponent,
+        CadastroTarefasComponent
     ],
     imports: [
         CommonModule,
@@ -22,7 +26,8 @@ import {MatCardTitle} from '@angular/material/card';
         MatIcon,
         MatBadge,
         MatCardTitle,
-        MatCardSubtitle
+        MatCardSubtitle,
+        MatButton
     ]
 })
 export class TarefaModule { }
